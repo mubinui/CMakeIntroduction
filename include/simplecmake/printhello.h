0c5c9133cce5fdff98a79785f1e67f1e3ad436a5
uicmu <mubin41@student.sust.edu>
2023-02-20 06:58:26 +0600

    Initial commit

---
+++ include/simplecmake/printhello.h
@@ -0,0 +1,5 @@
+//
+// Created by uicmu on 2/19/2023.
+//
+#pragma once
+void printhello();
---
+++ src/printhello.cpp
@@ -0,0 +1,10 @@
+//
+// Created by uicmu on 2/19/2023.
+
+//
+#include "iostream"
+#include "simplecmake/printhello.h"
+
+void printhello(){
+    std::cout<< "Hello MFS ";
+}
